1582bd3636f72abc9dfa1144797ccf26d2b246d7
SerSementsov <sersementsov@gmail.com>
2019-08-23 01:25:15 +0400

    finished Collections module

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,10 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = {}
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
+    return customers
+        .filter { customer ->
+            val (delivered, undelivered) = customer.orders.partition { it.isDelivered }
+
+            undelivered.size > delivered.size
+        }
+        .toSet()
+}
